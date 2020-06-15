5224283eff8cdab26329b98ebc39c9963b121f1b
sinuoc <45698324+sinuoc@users.noreply.github.com>
2020-06-15 01:34:47 -0700

    Update csv_process.py

--- csv_process.py
+++ csv_process.py
@@ -3,10 +3,11 @@ import math
 import numpy as np 
 import sys, os
 import pandas as pd
+import seaborn as sns
 from matpltlib import pyplot
 
 # 分类
-data=pd.DataFrame(columns=['Dominant_Topic','Publication_Year'])
+data=pd.DataFrame(columns=['Dominant_Topic','Publication_Year'], index=['1','2','3','4','5','6','7','8','9','']
 
 ## 注释掉的是旧代码
 
@@ -29,3 +30,5 @@ data=pd.DataFrame(columns=['Dominant_Topic','Publication_Year'])
 #pyplot.xticks([2001,2002,2003,2004,2005,2006,2007,2008,2009,2010,2011,2012,2013,2014,2015,2016,2017,2018,2019,2020], ["2001","2002","2003","2004","2005","2006","2007","2008","2009","2010","2011","2012","2013","2014","2015","2016","2017","2018","2019","2020"])
 #pyplot.xlabel("Year")
 #pyplot.show
+
+plt.rcParams['figure.figsize'] = (8.0, 6.0)
