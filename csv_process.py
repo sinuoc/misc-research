1ebc33a11c527ead184e821380cd45eddf67c302
sinuoc <45698324+sinuoc@users.noreply.github.com>
2020-06-15 01:29:07 -0700

    Update csv_process.py

--- csv_process.py
+++ csv_process.py
@@ -2,23 +2,30 @@ import csv
 import math
 import numpy as np 
 import sys, os
+import pandas as pd
 from matpltlib import pyplot
 
-listage = []
-listnum = []
+# 分类
+data=pd.DataFrame(columns=['Dominant_Topic','Publication_Year'])
 
-with open('dominant_topics_processed.csv','r',encoding='utf-8') as f:
-    reader = csv.reader(f)
-    header = next(reader)
+## 注释掉的是旧代码
 
-    for row in reader:
-        listage.append(row[0])
-        listnum.append(row[4104])
+# listage = []
+# listnum = []
+
+
+# with open('dominant_topics_processed.csv','r',encoding='utf-8') as f:
+    # reader = csv.reader(f)
+    # header = next(reader)
+
+    # for row in reader:
+        # listage.append(row[0])
+        # listnum.append(row[4104])
     
-x = listage
-y = listnum
+# x = listage
+# y = listnum
 
-pyplot.plot(x, y,)
-pyplot.xticks([2001,2002,2003,2004,2005,2006,2007,2008,2009,2010,2011,2012,2013,2014,2015,2016,2017,2018,2019,2020], ["2001","2002","2003","2004","2005","2006","2007","2008","2009","2010","2011","2012","2013","2014","2015","2016","2017","2018","2019","2020"])
-pyplot.xlabel("Year")
-pyplot.show
+#pyplot.plot(x, y,)
+#pyplot.xticks([2001,2002,2003,2004,2005,2006,2007,2008,2009,2010,2011,2012,2013,2014,2015,2016,2017,2018,2019,2020], ["2001","2002","2003","2004","2005","2006","2007","2008","2009","2010","2011","2012","2013","2014","2015","2016","2017","2018","2019","2020"])
+#pyplot.xlabel("Year")
+#pyplot.show
